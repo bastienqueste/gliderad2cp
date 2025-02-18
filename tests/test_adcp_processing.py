67653c9ccdd0539277b78433132be69ff0108c81
Callum Rollo <c.rollo@outlook.com>
2025-02-18 15:03:36 +0100

    streamline tests

--- tests/test_adcp_processing.py
+++ tests/test_adcp_processing.py
@@ -1,12 +1,11 @@
 import pandas as pd
 
 from gliderad2cp import process_currents, process_shear, tools, process_bias
-from gliderad2cp.download_example_data import data_source
+from gliderad2cp.download_example_data import load_sample_dataset
 import numpy as np
 
 options = tools.get_options(xaxis=2, verbose=True)
-profile_range = "160_to_210"
-glider_pqt_path = data_source.get_url(f"glider_profiles_{profile_range}.pqt")
+glider_pqt_path = load_sample_dataset(f"glider_profiles_160_to_210.pqt")
 
 data = pd.read_parquet(glider_pqt_path)
 data = data.rename({'dive_number': "dive_num"}, axis=1)
@@ -14,17 +13,14 @@ data = data[
     ["dead_reckoning", "time", "temperature", "salinity", "latitude", "pressure", "longitude", "profile_number",
      "declination", "dive_num"]]
 
-data_source.fetch(f"adcp_profiles_{profile_range}.nc")
-adcp_path = str(data_source.path / f"adcp_profiles_{profile_range}.nc")
+adcp_path = load_sample_dataset(f"adcp_profiles_160_to_210.nc")
 
 
 def test_process_shear():
-    adcp = process_shear.process(adcp_path, glider_pqt_path, options=options)
+    process_shear.process(adcp_path, glider_pqt_path, options=options)
 
 
 def test_process_currents():
-    adcp = process_shear.process(adcp_path, glider_pqt_path, options=options)
-
     gps_predive = []
     gps_postdive = []
 
@@ -49,8 +45,5 @@ def test_process_currents():
     gps_predive = np.vstack(gps_predive)
     gps_postdive = np.vstack(gps_postdive)
     ds_adcp = process_shear.process(adcp_path, data, options)
-    currents, DAC = process_currents.process(ds_adcp, gps_predive, gps_postdive, options)
-    bias_along_glider, bias_across_glider = process_bias.regress_bias(currents, options)
-
-    currents = process_bias.correct_bias(currents, options, bias_along_glider, bias_across_glider)
-
+    currents, dac = process_currents.process(ds_adcp, gps_predive, gps_postdive, options)
+    __ = process_bias.process(currents,options)
